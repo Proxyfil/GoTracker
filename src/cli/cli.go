a77338d5858aa268ab97eebf780fffa59b968e87
Proxyfil <piloupilou200@gmail.com>
2025-04-08 09:51:36 +0200

    fix : Changed way CLI get commands

--- src/cli/cli.go
+++ src/cli/cli.go
@@ -7,18 +7,33 @@ import (
 )
 
 func Open() {
+	// Define the reader to read from standard input
 	reader := bufio.NewReader(os.Stdin)
 
+	// While loop to keep the CLI running
 	for {
 		fmt.Print("Command : ");
+		// Read the command from standard input
 		command, _ := reader.ReadString('\n');
-		fmt.Print(command);
 
-		if command == "exit\n" {
-			break
+		// Switch between commands
+		switch command {
+		case "exit\n":
+			fmt.Println("Exiting CLI...")
+			os.Exit(0)
+
+		case "help\n":
+			fmt.Println("Available commands:")
+			fmt.Println("  - exit: Exit the CLI")
+			fmt.Println("  - help: Show this help message")
+
+		default:
+			fmt.Println("Unknown command. Type 'help' for a list of commands.")
 		}
 	}
 
+	// Close the reader
+	defer reader.Close()
 	fmt.Println("Exiting CLI...")
 	os.Exit(0)
 }
