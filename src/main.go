41fc31a8db8c16657bb7a8e55d2dfcc431cc2e6a
Proxyfil <piloupilou200@gmail.com>
2025-04-16 18:07:53 +0200

    feat : Created delete, history, link and list commands

--- src/main.go
+++ src/main.go
@@ -260,6 +260,445 @@ func main() {
 					}
 					fmt.Printf("User %s %s logged in successfully with ID: %d\n", user.Firstname, user.Lastname, user.ID)
 
+				case "add":
+					if(user.ID == 0){
+						fmt.Println("Please login or register first.")
+						fmt.Print("Command: ")
+						continue
+					}
+					// We get arguments from the command
+					addArgs := strings.Split(msg.Command, " ")
+					if len(addArgs) < 2 {
+						fmt.Println("Please provide type, can be 'food', 'meal' or 'day'")
+						fmt.Print("Command: ")
+						continue
+					}
+					addType := addArgs[1]
+				
+					switch addType {
+						case "food":
+							if len(addArgs) < 4 {
+								fmt.Println("Please provide a food ID and quantity to add.")
+								fmt.Print("Command: ")
+								continue
+							}
+							foodID, err := strconv.Atoi(addArgs[2])
+							if err != nil {
+								fmt.Println("Error converting food ID to int:", err)
+								fmt.Print("Command: ")
+								continue
+							}
+							quantity, err := strconv.Atoi(addArgs[3])
+							if err != nil {
+								fmt.Println("Error converting quantity to int:", err)
+								fmt.Print("Command: ")
+								continue
+							}
+
+							// Save the food history to the database
+							current_time := time.Now().Local()
+							err = db.AddFoodHistory(database, user.ID, foodID, current_time.Format("2006-01-02"), quantity)
+							if err != nil {
+								fmt.Println("Error saving food history:", err)
+								fmt.Print("Command: ")
+								continue
+							} else {
+								fmt.Println("Food history saved successfully.")
+							}
+						
+						case "meal":
+							if len(addArgs) < 3 {
+								fmt.Println("Please provide a meal ID to add.")
+								fmt.Print("Command: ")
+								continue
+							}
+							mealID, err := strconv.Atoi(addArgs[2])
+							if err != nil {
+								fmt.Println("Error converting meal ID to int:", err)
+								fmt.Print("Command: ")
+								continue
+							}
+							// Get food IDs associated with the meal
+							foods, err := db.GetFoodWithMeal(database, mealID)
+							if err != nil {
+								fmt.Println("Error fetching food IDs with meal:", err)
+								fmt.Print("Command: ")
+								continue
+							}
+							if len(foods) == 0 {
+								fmt.Println("No food IDs found for the specified meal.")
+								fmt.Print("Command: ")
+								continue
+							}
+							// Save the food history to the database
+							current_time := time.Now().Local()
+							for _, food := range foods{
+								err = db.AddFoodHistory(database, user.ID, food[0], current_time.Format("2006-01-02"), food[1])
+								if err != nil {
+									fmt.Println("Error saving food history:", err)
+									fmt.Print("Command: ")
+									continue
+								} else {
+									fmt.Printf("Food history for food ID %d saved successfully.\n", food[1])
+								}
+							}
+
+						case "day":
+							if len(addArgs) < 3 {
+								fmt.Println("Please provide a day ID to add.")
+								fmt.Print("Command: ")
+								continue
+							}
+							dayID, err := strconv.Atoi(addArgs[2])
+							if err != nil {
+								fmt.Println("Error converting day ID to int:", err)
+								fmt.Print("Command: ")
+								continue
+							}
+							// Get meal IDs associated with the day
+							meals, err := db.GetMealWithDayPreset(database, dayID)
+							if err != nil {
+								fmt.Println("Error fetching meal IDs with day:", err)
+								fmt.Print("Command: ")
+								continue
+							}
+							if len(meals) == 0 {
+								fmt.Println("No meal IDs found for the specified day.")
+								fmt.Print("Command: ")
+								continue
+							}
+							// Save the food history to the database
+							current_time := time.Now().Local()
+							for _, meal := range meals {
+								foods, err := db.GetFoodWithMeal(database, meal[0])
+								if err != nil {
+									fmt.Println("Error fetching food IDs with meal:", err)
+									fmt.Print("Command: ")
+									continue
+								}
+								if len(foods) == 0 {
+									fmt.Println("No food IDs found for the specified meal.")
+									fmt.Print("Command: ")
+									continue
+								}
+								for _, food := range foods {
+									err = db.AddFoodHistory(database, user.ID, food[0], current_time.Format("2006-01-02"), food[1]*meal[1])
+									if err != nil {
+										fmt.Println("Error saving food history:", err)
+										fmt.Print("Command: ")
+										continue
+									} else {
+										fmt.Printf("Food history for food ID %d saved successfully.\n", food[0])
+									}
+								}
+							}
+
+						default:
+							fmt.Println("Unknown add type. Please use 'food', 'meal' or 'day'.")
+					}
+
+				case "create":
+					if(user.ID == 0){
+						fmt.Println("Please login or register first.")
+						fmt.Print("Command: ")
+						continue
+					}
+					// We get arguments from the command
+					createArgs := strings.Split(msg.Command, " ")
+					if len(createArgs) < 2 {
+						fmt.Println("Please provide a type, can be 'meal' or 'day'")
+						fmt.Print("Command: ")
+						continue
+					}
+					createType := createArgs[1]
+					switch createType {
+						case "meal":
+							if len(createArgs) < 4 {
+								fmt.Println("Please provide a meal name and type to create.")
+								fmt.Print("Command: ")
+								continue
+							}
+							mealName := createArgs[2]
+							mealType := createArgs[3]
+							// Create a new meal in the database
+							mealID, err := db.CreateMeal(database, mealName, mealType)
+							if err != nil {
+								fmt.Println("Error creating meal:", err)
+								fmt.Print("Command: ")
+								continue
+							}
+							fmt.Printf("Meal '%s' created successfully with ID: %d\n", mealName, mealID)
+						case "day":
+							if len(createArgs) < 3 {
+								fmt.Println("Please provide a day name to create.")
+								fmt.Print("Command: ")
+								continue
+							}
+							dayName := createArgs[2]
+							// Create a new day in the database
+							dayID, err := db.CreateDayPreset(database, user.ID, dayName)
+							if err != nil {
+								fmt.Println("Error creating day:", err)
+								fmt.Print("Command: ")
+								continue
+							}
+							fmt.Printf("Day '%s' created successfully with ID: %d\n", dayName, dayID)
+						default:
+							fmt.Println("Unknown create type. Please use 'meal' or 'day'.")
+					}
+				case "list":
+					// We get arguments from the command
+					listArgs := strings.Split(msg.Command, " ")
+					if len(listArgs) < 2 {
+						fmt.Println("Please provide a type, can be 'meal' or 'day'")
+						fmt.Print("Command: ")
+						continue
+					}
+					listType := listArgs[1]
+					switch listType {
+						case "meal":
+							// List all meals in the database
+							meals, err := db.GetAllMeals(database)
+							if err != nil {
+								fmt.Println("Error fetching meals:", err)
+								fmt.Print("Command: ")
+								continue
+							}
+							if len(meals) == 0 {
+								fmt.Println("No meals found.")
+							} else {
+								fmt.Println("Meals:")
+								for _, meal := range meals {
+									fmt.Println(" -", meal)
+								}
+							}
+						case "day":
+							// List all days in the database
+							days, err := db.GetAllDays(database)
+							if err != nil {
+								fmt.Println("Error fetching days:", err)
+								fmt.Print("Command: ")
+								continue
+							}
+							if len(days) == 0 {
+								fmt.Println("No days found.")
+							} else {
+								fmt.Println("Days:")
+								for _, day := range days {
+									fmt.Println(" -", day)
+								}
+							}
+						default:
+							fmt.Println("Unknown list type. Please use 'meal' or 'day'.")
+					}
+
+				case "link":
+					if(user.ID == 0){
+						fmt.Println("Please login or register first.")
+						fmt.Print("Command: ")
+						continue
+					}
+					// We get arguments from the command
+					linkArgs := strings.Split(msg.Command, " ")
+					if len(linkArgs) < 2 {
+						fmt.Println("Please provide a type, can be 'food_to_meal' or 'meal_to_day'")
+						fmt.Print("Command: ")
+						continue
+					}
+					linkType := linkArgs[1]
+					switch linkType {
+						case "food_to_meal":
+							if len(linkArgs) < 5 {
+								fmt.Println("Please provide a food ID, meal ID and quantity to link.")
+								fmt.Print("Command: ")
+								continue
+							}
+							foodID, err := strconv.Atoi(linkArgs[2])
+							if err != nil {
+								fmt.Println("Error converting food ID to int:", err)
+								fmt.Print("Command: ")
+								continue
+							}
+							mealID, err := strconv.Atoi(linkArgs[3])
+							if err != nil {
+								fmt.Println("Error converting meal ID to int:", err)
+								fmt.Print("Command: ")
+								continue
+							}
+							quantity, err := strconv.Atoi(linkArgs[4])
+							if err != nil {
+								fmt.Println("Error converting quantity to int:", err)
+								fmt.Print("Command: ")
+								continue
+							}
+							// Link food to meal in the database
+							err = db.LinkFoodToMeal(database, foodID, mealID, quantity)
+							if err != nil {
+								fmt.Println("Error linking food to meal:", err)
+								fmt.Print("Command: ")
+								continue
+							} else {
+								fmt.Printf("Food ID %d linked to Meal ID %d successfully.\n", foodID, mealID)
+							}
+						case "meal_to_day":
+							if len(linkArgs) < 5 {
+								fmt.Println("Please provide a meal ID, day ID and quantity to link.")
+								fmt.Print("Command: ")
+								continue
+							}
+							mealID, err := strconv.Atoi(linkArgs[2])
+							if err != nil {
+								fmt.Println("Error converting meal ID to int:", err)
+								fmt.Print("Command: ")
+								continue
+							}
+							dayID, err := strconv.Atoi(linkArgs[3])
+							if err != nil {
+								fmt.Println("Error converting day ID to int:", err)
+								fmt.Print("Command: ")
+								continue
+							}
+							quantity, err := strconv.Atoi(linkArgs[4])
+							if err != nil {
+								fmt.Println("Error converting quantity to int:", err)
+								fmt.Print("Command: ")
+								continue
+							}
+							// Link meal to day in the database
+							err = db.LinkMealToDayPreset(database, mealID, dayID, quantity)
+							if err != nil {
+								fmt.Println("Error linking meal to day:", err)
+								fmt.Print("Command: ")
+								continue
+							} else {
+								fmt.Printf("Meal ID %d linked to Day ID %d successfully.\n", mealID, dayID)
+							}
+						default:
+							fmt.Println("Unknown link type. Please use 'food_to_meal' or 'meal_to_day'.")
+					}
+
+				case "history":
+					if(user.ID == 0){
+						fmt.Println("Please login or register first.")
+						fmt.Print("Command: ")
+						continue
+					}
+					// We get arguments from the command
+					historyArgs := strings.Split(msg.Command, " ")
+					if len(historyArgs) < 2 {
+						fmt.Println("Please provide a type, can be 'food', 'weight', 'imc' or 'bodyfat'")
+						fmt.Print("Command: ")
+						continue
+					}
+					historyType := historyArgs[1]
+					switch historyType {
+						case "food":
+							// Get food history for the user
+							foodHistory, err := db.GetFoodHistory(database, user.ID)
+							if err != nil {
+								fmt.Println("Error fetching food history:", err)
+								fmt.Print("Command: ")
+								continue
+							}
+							if len(foodHistory) == 0 {
+								fmt.Println("No food history found.")
+							} else {
+								fmt.Println("Food History:")
+								for _, food := range foodHistory {
+									fmt.Println(" - Date:", food[1], "| Food ID:", food[0], "| Quantity:", food[2], "| Entry ID:", food[3])
+								}
+							}
+						case "weight":
+							// Get weight history for the user
+							weightHistory, err := db.GetWeightHistory(database, user.ID)
+							if err != nil {
+								fmt.Println("Error fetching weight history:", err)
+								fmt.Print("Command: ")
+								continue
+							}
+							if len(weightHistory) == 0 {
+								fmt.Println("No weight history found.")
+							} else {
+								fmt.Println("Weight History:")
+								for _, weight := range weightHistory {
+									fmt.Println(" - Date:", weight[0], "| Weight:", weight[1])
+								}
+							}
+						case "imc":
+							// Get IMC history for the user
+							imcHistory, err := db.GetIMCHistory(database, user.ID)
+							if err != nil {
+								fmt.Println("Error fetching IMC history:", err)
+								fmt.Print("Command: ")
+								continue
+							}
+							if len(imcHistory) == 0 {
+								fmt.Println("No IMC history found.")
+							} else {
+								fmt.Println("IMC History:")
+								for _, imc := range imcHistory {
+									fmt.Println(" - Date:", imc[0], "| IMC:", imc[1])
+								}
+							}
+						case "bodyfat":
+							// Get body fat history for the user
+							bodyFatHistory, err := db.GetBodyFatHistory(database, user.ID)
+							if err != nil {
+								fmt.Println("Error fetching body fat history:", err)
+								fmt.Print("Command: ")
+								continue
+							}
+							if len(bodyFatHistory) == 0 {
+								fmt.Println("No body fat history found.")
+							} else {
+								fmt.Println("Body Fat History:")
+								for _, bodyFat := range bodyFatHistory {
+									fmt.Println(" - Date:", bodyFat[0], "| Body Fat:", bodyFat[1])
+								}
+							}
+						default:
+							fmt.Println("Unknown history type. Please use 'food', 'weight', 'imc' or 'bodyfat'.")
+					}
+				case "delete":
+					if(user.ID == 0){
+						fmt.Println("Please login or register first.")
+						fmt.Print("Command: ")
+						continue
+					}
+					// We get arguments from the command
+					deleteArgs := strings.Split(msg.Command, " ")
+					if len(deleteArgs) < 2 {
+						fmt.Println("Please provide a type, can be 'food'.")
+						fmt.Print("Command: ")
+						continue
+					}
+					deleteType := deleteArgs[1]
+					switch deleteType {
+						case "food":
+							if len(deleteArgs) < 3 {
+								fmt.Println("Please provide a Entry ID to delete.")
+								fmt.Print("Command: ")
+								continue
+							}
+							entryID, err := strconv.Atoi(deleteArgs[2])
+							if err != nil {
+								fmt.Println("Error converting entry ID to int:", err)
+								fmt.Print("Command: ")
+								continue
+							}
+							// Delete food history from the database
+							err = db.DeleteFoodHistory(database, entryID)
+							if err != nil {
+								fmt.Println("Error deleting food history:", err)
+								fmt.Print("Command: ")
+								continue
+							} else {
+								fmt.Printf("Food history with Entry ID %d deleted successfully.\n", entryID)
+							}
+						default:
+							fmt.Println("Unknown delete type. Please use 'food'.")
+					}
+
 				case "exit":
 					fmt.Println("Shutting down the application...")
 					return
--- src/utils/db.go
+++ src/utils/db.go
@@ -114,30 +114,6 @@ func Migrate(db *sql.DB) error {
 		return fmt.Errorf("failed to create weight_history table: %w", err)
 	}
 
-	// Create food table if it doesn't exist
-	_, err = db.Exec(`
-		CREATE TABLE IF NOT EXISTS food (
-			id SERIAL PRIMARY KEY,
-			name VARCHAR(50),
-			fat FLOAT,
-			saturated_fat FLOAT,
-			trans_fat FLOAT,
-			cholesterol FLOAT,
-			sodium FLOAT,
-			carbohydrates FLOAT,
-			fiber FLOAT,
-			sugars FLOAT,
-			protein FLOAT,
-			calcium FLOAT,
-			iron FLOAT,
-			potassium FLOAT,
-			calories FLOAT
-		)
-	`)
-	if err != nil {
-		return fmt.Errorf("failed to create food table: %w", err)
-	}
-
 	// Create meal table if it doesn't exist
 	_, err = db.Exec(`
 		CREATE TABLE IF NOT EXISTS meal (
@@ -155,7 +131,7 @@ func Migrate(db *sql.DB) error {
 		CREATE TABLE IF NOT EXISTS meal_food (
 			id SERIAL PRIMARY KEY,
 			meal_id INT REFERENCES meal(id),
-			food_id INT REFERENCES food(id),
+			food_id INT,
 			quantity FLOAT
 		)
 	`)
@@ -168,7 +144,7 @@ func Migrate(db *sql.DB) error {
 		CREATE TABLE IF NOT EXISTS food_history (
 			id SERIAL PRIMARY KEY,
 			user_id INT REFERENCES users(id),
-			food_id INT REFERENCES food(id),
+			food_id INT,
 			date DATE,
 			quantity FLOAT
 		)
@@ -288,4 +264,266 @@ func GetUser(db *sql.DB, userID int) (int, string, string, int, int, int, int, e
 		return 0, "", "", 0, 0, 0, 0, fmt.Errorf("failed to get user: %w", err)
 	}
 	return id, firstname, lastname, age, weight, height, targetWeight, nil
+}
+
+func AddFoodHistory(db *sql.DB, userID int, foodID int, date string, quantity int) error {
+	_, err := db.Exec(`
+		INSERT INTO food_history (user_id, food_id, date, quantity)
+		VALUES ($1, $2, $3, $4)
+	`, userID, foodID, date, quantity)
+	if err != nil {
+		return fmt.Errorf("failed to insert food history: %w", err)
+	}
+	return nil
+}
+
+func GetFoodWithMeal(db *sql.DB, mealID int) ([][2]int, error) {
+	rows, err := db.Query(`
+		SELECT food_id, quantity
+		FROM meal_food
+		WHERE meal_id = $1
+	`, mealID)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get food with meal: %w", err)
+	}
+	defer rows.Close()
+
+	var foods [][2]int
+	for rows.Next() {
+		var foodID int
+		var quantity int
+		if err := rows.Scan(&foodID, &quantity); err != nil {
+			return nil, fmt.Errorf("failed to scan food ID: %w", err)
+		}
+		foods = append(foods, [2]int{foodID, quantity})
+	}
+
+	return foods, nil
+}
+
+func GetMealWithDayPreset(db *sql.DB, dayPresetID int) ([][2]int, error) {
+	rows, err := db.Query(`
+		SELECT meal_id, quantity
+		FROM day_preset_meal
+		WHERE day_preset_id = $1
+	`, dayPresetID)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get meal with day preset: %w", err)
+	}
+	defer rows.Close()
+
+	var meals [][2]int
+	for rows.Next() {
+		var mealID int
+		var quantity int
+		if err := rows.Scan(&mealID, &quantity); err != nil {
+			return nil, fmt.Errorf("failed to scan meal ID: %w", err)
+		}
+		meals = append(meals, [2]int{mealID, quantity})
+	}
+
+	return meals, nil
+}
+
+func CreateMeal(db *sql.DB, name string, mealType string) (int, error) {
+	var mealID int
+	err := db.QueryRow(`
+		INSERT INTO meal (name, type)
+		VALUES ($1, $2)
+		RETURNING id
+	`, name, mealType).Scan(&mealID)
+	if err != nil {
+		return 0, fmt.Errorf("failed to insert meal: %w", err)
+	}
+	return mealID, nil
+}
+
+func CreateDayPreset(db *sql.DB, userID int, name string) (int, error) {
+	var dayPresetID int
+	err := db.QueryRow(`
+		INSERT INTO day_preset (user_id, name)
+		VALUES ($1, $2)
+		RETURNING id
+	`, userID, name).Scan(&dayPresetID)
+	if err != nil {
+		return 0, fmt.Errorf("failed to insert day preset: %w", err)
+	}
+	return dayPresetID, nil
+}
+
+func GetAllMeals(db *sql.DB) ([]string, error) {
+	rows, err := db.Query(`
+		SELECT id, name, type
+		FROM meal
+	`)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get all meals: %w", err)
+	}
+	defer rows.Close()
+
+	var meals []string
+	for rows.Next() {
+		var id int
+		var name, mealType string
+		if err := rows.Scan(&id, &name, &mealType); err != nil {
+			return nil, fmt.Errorf("failed to scan meal: %w", err)
+		}
+		meals = append(meals, fmt.Sprintf("ID: %d | Name: %s | Type: %s", id, name, mealType))
+	}
+
+	return meals, nil
+}
+
+func GetAllDays(db *sql.DB) ([]string, error) {
+	rows, err := db.Query(`
+		SELECT id, name
+		FROM day_preset
+	`)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get all day presets: %w", err)
+	}
+	defer rows.Close()
+
+	var dayPresets []string
+	for rows.Next() {
+		var id int
+		var name string
+		if err := rows.Scan(&id, &name); err != nil {
+			return nil, fmt.Errorf("failed to scan day preset: %w", err)
+		}
+		dayPresets = append(dayPresets, fmt.Sprintf("ID: %d | Name: %s", id, name))
+	}
+
+	return dayPresets, nil
+}
+
+func LinkFoodToMeal(db *sql.DB, foodID int, mealID int, quantity int) error {
+	_, err := db.Exec(`
+		INSERT INTO meal_food (meal_id, food_id, quantity)
+		VALUES ($1, $2, $3)
+	`, mealID, foodID, quantity)
+	if err != nil {
+		return fmt.Errorf("failed to link food to meal: %w", err)
+	}
+	return nil
+}
+
+func LinkMealToDayPreset(db *sql.DB, mealID int, dayPresetID int, quantity int) error {
+	_, err := db.Exec(`
+		INSERT INTO day_preset_meal (day_preset_id, meal_id, quantity)
+		VALUES ($1, $2, $3)
+	`, dayPresetID, mealID, quantity)
+	if err != nil {
+		return fmt.Errorf("failed to link meal to day preset: %w", err)
+	}
+	return nil
+}
+
+func GetFoodHistory(db *sql.DB, userID int) ([][4]interface{}, error) {
+	rows, err := db.Query(`
+		SELECT food_id, date, quantity, id
+		FROM food_history
+		WHERE user_id = $1
+	`, userID)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get food history: %w", err)
+	}
+	defer rows.Close()
+
+	var foodHistory [][4]interface{}
+	for rows.Next() {
+		var foodID int
+		var date string
+		var quantity float64
+		var entryID int
+		if err := rows.Scan(&foodID, &date, &quantity, &entryID); err != nil {
+			return nil, fmt.Errorf("failed to scan food history: %w", err)
+		}
+		foodHistory = append(foodHistory, [4]interface{}{foodID, date, quantity, entryID})
+	}
+
+	return foodHistory, nil
+}
+
+func GetWeightHistory(db *sql.DB, userID int) ([][3]interface{}, error) {
+	rows, err := db.Query(`
+		SELECT date, weight
+		FROM weight_history
+		WHERE user_id = $1
+	`, userID)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get weight history: %w", err)
+	}
+	defer rows.Close()
+
+	var weightHistory [][3]interface{}
+	for rows.Next() {
+		var date string
+		var weight int
+		if err := rows.Scan(&date, &weight); err != nil {
+			return nil, fmt.Errorf("failed to scan weight history: %w", err)
+		}
+		weightHistory = append(weightHistory, [3]interface{}{date, weight})
+	}
+
+	return weightHistory, nil
+}
+
+func GetBodyFatHistory(db *sql.DB, userID int) ([][3]interface{}, error) {
+	rows, err := db.Query(`
+		SELECT date, body_fat
+		FROM body_fat_history
+		WHERE user_id = $1
+	`, userID)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get body fat history: %w", err)
+	}
+	defer rows.Close()
+
+	var bodyFatHistory [][3]interface{}
+	for rows.Next() {
+		var date string
+		var bodyFat float64
+		if err := rows.Scan(&date, &bodyFat); err != nil {
+			return nil, fmt.Errorf("failed to scan body fat history: %w", err)
+		}
+		bodyFatHistory = append(bodyFatHistory, [3]interface{}{date, bodyFat})
+	}
+
+	return bodyFatHistory, nil
+}
+
+func GetIMCHistory(db *sql.DB, userID int) ([][3]interface{}, error) {
+	rows, err := db.Query(`
+		SELECT date, imc
+		FROM imc_history
+		WHERE user_id = $1
+	`, userID)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get IMC history: %w", err)
+	}
+	defer rows.Close()
+
+	var imcHistory [][3]interface{}
+	for rows.Next() {
+		var date string
+		var imc float64
+		if err := rows.Scan(&date, &imc); err != nil {
+			return nil, fmt.Errorf("failed to scan IMC history: %w", err)
+		}
+		imcHistory = append(imcHistory, [3]interface{}{date, imc})
+	}
+
+	return imcHistory, nil
+}
+
+func DeleteFoodHistory(db *sql.DB, entryID int) error {
+	_, err := db.Exec(`
+		DELETE FROM food_history
+		WHERE id = $1
+	`, entryID)
+	if err != nil {
+		return fmt.Errorf("failed to delete food history: %w", err)
+	}
+	return nil
 }
